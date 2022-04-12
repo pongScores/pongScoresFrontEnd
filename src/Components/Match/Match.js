ee858d503586496a40e6aa01fa866c75e6595c56
pspyoung <pspyoung2@gmail.com>
2022-04-11 17:00:34 -0700

    Winner and Loser States updates on inspector tools

--- src/Components/Match/Match.js
+++ src/Components/Match/Match.js
@@ -14,22 +14,31 @@ const playerTwo = {
 };
 
 function Match() {
+	let winner;
+	let loser;
 	const initialState = {
 		winner: '',
 		loser: '',
 	};
 
-	const submitMatch = (event) => {
+	const [selectState, setSelectState] = useState(initialState);
+
+	const handleSubmit = (event) => {
 		event.preventDefault();
 		console.log(selectState);
-
 		setSelectState(initialState);
-	};
-	function submitMatch() {}
-	// winner.wins = winner.wins + 1;
-	// loser.losses = loser.losses + 1;
+		// let winner = selectWinner();
+		// let loser = selectLoser();
 
-	const [selectState, setSelectState] = useState(initialState);
+		console.log(playerOne);
+		console.log(playerTwo);
+	};
+
+	const handleChange = (event) => {
+		console.log(event.target.id);
+		setSelectState({ ...selectState, [event.target.id]: event.target.value });
+	};
+
 	const [wins, setWins] = useState(0);
 	const [losses, setLosses] = useState(0);
 
@@ -43,32 +52,50 @@ function Match() {
 		setLosses(playerLost);
 	}
 
+	// const selectWinner = document.getElementById('winner').value;
+
+	// const selectLoser = document.getElementById('loser').value;
+
+	// this.handleChange = this.handleChange.bind(this);
+	// this.handleSubmit = this.handleSubmit.bind(this);
+
 	useEffect(() => {}, []);
 
 	return (
 		<div>
-			<h4>
-				Winner:
-				<select name="players" id="winner">
-					<option value="select"> Select Player </option>
-					<option value={playerOne.name}> {playerOne.name} </option>
-					<option value={playerTwo.name}> {playerTwo.name} </option>
-				</select>
-			</h4>
-			<h4>
-				Loser:
-				<select name="players" id="loser">
-					<option value="select"> Select Player </option>
-					<option value={playerOne.name}> {playerOne.name} </option>
-					<option value={playerTwo.name}> {playerTwo.name} </option>
-				</select>
-			</h4>
-
-			{/* submit logic here */}
-			<button type="submit">Submit</button>
-			{/* onclick, invoke winner/loser function */}
-			{/* if winner, wins+1 */}
-			{/* if loser, losses + 1 */}
+			<form onSubmit={handleSubmit}>
+				<h3>
+					<label for="winner"> Winner:</label>
+					<select
+						name="players"
+						id="winner"
+						onChange={handleChange}
+						value={selectState.winner}>
+						<option value="select"> Select Player </option>
+						<option value={playerOne.name}> {playerOne.name} </option>
+						<option value={playerTwo.name}> {playerTwo.name} </option>
+					</select>
+				</h3>
+				<h3>
+					<label for="loser"> Loser:</label>
+					<select
+						name="players"
+						id="loser"
+						onChange={handleChange}
+						value={selectState.loser}>
+						<option value="select"> Select Player </option>
+						<option value={playerOne.name}> {playerOne.name} </option>
+						<option value={playerTwo.name}> {playerTwo.name} </option>
+					</select>
+				</h3>
+				{/* submit logic here */}
+				<button type="submit" onClick={handleSubmit}>
+					Submit
+				</button>
+				{/* onclick, invoke winner/loser function */}
+				{/* if winner, wins+1 */}
+				{/* if loser, losses + 1 */}
+			</form>
 		</div>
 	);
 }
