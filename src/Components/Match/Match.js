51cec9d1f27f1a7f05f837e2ea506ac77ca870fa
pspyoung <pspyoung2@gmail.com>
2022-04-12 21:40:40 -0700

    Fixes ReactDOM.render is no longer supported in React 18 error message. Changes players' data to array, and the way it's called in select form. Adds psuedocode and comments to add wins

--- src/Components/Match/Match.js
+++ src/Components/Match/Match.js
@@ -1,77 +1,111 @@
 import React, { useState, useEffect } from 'react';
 import API_URL from '../../apiConfig';
 // put states for players
-let playerOne = {
-	name: 'Kemba',
-	rating: 1600,
-	wins: 0,
-	losses: 0,
-};
-let playerTwo = {
-	name: 'Krenko',
-	rating: 1400,
-	wins: 0,
-	losses: 0,
-};
+let players = [
+	{
+		name: 'Kemba',
+		rating: 1600,
+		wins: 0,
+		losses: 0,
+	},
+	{
+		name: 'Krenko',
+		rating: 1400,
+		wins: 0,
+		losses: 0,
+	},
+];
 
 // target player one and player two instead of win
 // if player 1 = winner, player 1.wins +1
 // on submit, pass back player 1 or player 2 instead of names
 
 function Match(props) {
-	let winner;
-	let loser;
-
 	const initialState = {
 		winner: '',
 		loser: '',
 	};
 
+	const initialPlayerState = {
+		name: '',
+		rating: 0,
+		wins: 0,
+		losses: 0,
+	};
+
+	// initializing playerOne and playerTwo states, the states to be set once dropdown menu selects player name
+	// setPlayer state will use the state of player's name selected
+
+	const [playerOneState, setPlayerOneState] = useState(initialPlayerState);
+	const [playerTwoState, setPlayerTwoState] = useState(initialPlayerState);
+
 	const [selectState, setSelectState] = useState(initialState);
+	// playersData for API data
 	const [playersData, setPlayersData] = useState([]);
 
 	const [wins, setWins] = useState(0);
 	const [losses, setLosses] = useState(0);
 
-	const countWins = () => {
-		// if winner's name matches a name that exists,
-		// add a win
-		// let playerWon = wins + 1;
-		// setWins(playerWon);
-		setWins(wins + 1);
-		// console.log(countWins);
-	};
+	/* const array = [1, 2, 3, 4, 5];
 
-	function countLosses() {
-		let playerLost = losses + 1;
-		setLosses(playerLost);
-	}
+// checks whether an element is even
+const even = (element) => element % 2 === 0;
+
+console.log(array.some(even));
+// expected output: true
+ */
+
+	/* const arr = [{ id: 1, username: 'fred' }, { id: 2, username: 'bill' }, { id: 3, username: 'ted' }];
+
+function add(arr, name) {
+  const { length } = arr;
+  const id = length + 1;
+  const found = arr.some(el => el.username === name);
+  if (!found) arr.push({ id, username: name });
+  return arr;
+}
+
+console.log(add(arr, 'ted')); */
+
+	const chooseLoser = () => {};
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		console.log(selectState);
 		// console.log(selectState.winner, 'wins');
 		console.log(selectState.winner);
-
-		countWins();
+		// chooseWinner();
+		// addWins();
 
 		setSelectState(initialState);
+		console.log(players);
 		// let winner = selectWinner();
 		// let loser = selectLoser();
-
-		console.log(playerOne);
-		console.log(playerTwo);
 	};
 
 	const handleChange = (event) => {
-		console.log(event.target.id);
+		// console.log(event.target.id);
 		setSelectState({ ...selectState, [event.target.id]: event.target.value });
 		console.log(event.target.value);
-		console.log(event.target);
-	};
+		// chooseWinner();
 
-	// Brainstorm solutions to update wins/losses
-	// If/Else statements ;
-	// If name of winner === name of somebody in list, add wins
+		// set playerOne state to the the array index in players that matches the name of winner
+
+		// set playerTwo state to the the array index in players that matches the name of loser
+
+		// use array.some method to find if a match exists
+	};
+	// const chooseWinner = (event, name) => {
+	// 	const winner = players.some((element) => element.name === name);
+	// 	if (event.target.value === winner) {
+	// 		setPlayerOneState(winner);
+	// 	}
+	// };
+
+	// const addWins = (playerOneState) => {
+	// 	playerOneState.wins = playerOneState.wins + 1;
+	// 	console.log(players);
+	// };
 
 	const listPlayers = () => {};
 
@@ -95,8 +129,8 @@ function Match(props) {
 						onChange={handleChange}
 						value={selectState.winner}>
 						<option value="select"> Select Player </option>
-						<option value={playerOne.name}> {playerOne.name} </option>
-						<option value={playerTwo.name}> {playerTwo.name} </option>
+						<option value={players[0].name}> {players[0].name} </option>
+						<option value={players[1].name}> {players[1].name} </option>
 					</select>
 				</h3>
 				<h3>
@@ -107,11 +141,11 @@ function Match(props) {
 						onChange={handleChange}
 						value={selectState.loser}>
 						<option value="select"> Select Player </option>
-						<option value={playerOne.name}> {playerOne.name} </option>
-						<option value={playerTwo.name}> {playerTwo.name} </option>
+						<option value={players[0].name}> {players[0].name} </option>
+						<option value={players[1].name}> {players[1].name} </option>
 					</select>
 				</h3>
-				<button type="submit" onClick={(handleSubmit, countWins, countLosses)}>
+				<button type="submit" onClick={handleSubmit}>
 					Submit
 				</button>
 			</form>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
 	<Router>
 		<App />
-	</Router>,
-	document.getElementById('root')
+	</Router>
+	// document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
