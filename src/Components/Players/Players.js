6bbfbf44103951cd593455ca179ac813ded2b35e
pspyoung <pspyoung2@gmail.com>
2022-04-13 14:41:10 -0700

    Fixes bug with React: 'Each child in a list should have a unique 'key' prop.'

--- src/Components/Players/Players.js
+++ src/Components/Players/Players.js
@@ -9,10 +9,14 @@ function Players(props) {
 		<div className="players-list">
 			{props.playersData.map((element) => {
 				return (
-					<div element={element} key={element.id} className="playersLinksBody">
+					<div
+						element={element}
+						key={element.name}
+						className="playersLinksBody">
 						{/* <Link to={'/players/' + element.name}>{element.name} </Link> */}
 						<p>
-							{element.name} {element.rating}
+							{element.name} {element.rating} Wins: {element.wins} Losses:
+							{element.losses}
 						</p>
 					</div>
 				);
