946b7242d19a644ef97a8e8078d59c70ebb976c9
pspyoung <pspyoung2@gmail.com>
2022-04-14 16:23:34 -0700

    Fixes put route functionality to update wins and losses with submit button. Cleans up unused code and comments

--- src/Components/Match/Match.js
+++ src/Components/Match/Match.js
@@ -3,22 +3,7 @@ import './Match.css';
 import API_URL from '../../apiConfig';
 import axios from 'axios';
 
-let players = [
-	{
-		name: 'Kemba',
-		rating: 1600,
-		wins: 0,
-		losses: 0,
-	},
-	{
-		name: 'Krenko',
-		rating: 1400,
-		wins: 0,
-		losses: 0,
-	},
-];
-
-function Match(data, props) {
+function Match(props) {
 	useEffect(() => {
 		axios(API_URL)
 			.then(({ data }) => {
@@ -52,56 +37,19 @@ function Match(data, props) {
 
 	const listPlayers = async () => {
 		try {
-			const response = await axios
-				.get(API_URL)
-				.then((response) => setPlayersList(response.data));
+			const response = await axios.get(API_URL);
+			setPlayersList(response.data);
 			console.log(response.data);
 		} catch (error) {
 			console.log(error);
 		}
-		// console.log(playersList);
 	};
-	// listPlayers();
-
-	// const [playerOneState, setPlayerOneState] = useState(initialPlayerState);
-	// const [playerTwoState, setPlayerTwoState] = useState(initialPlayerState);
 
 	const [selectState, setSelectState] = useState(initialState);
 	// playersData for API data
 	const [playersData, setPlayersData] = useState([]);
 	const [options, setOptions] = useState([]);
 
-	// const [wins, setWins] = useState(0);
-	// const [losses, setLosses] = useState(0);
-
-	/* 	const handleSubmit = (event) => {
-		event.preventDefault();
-		console.log(selectState);
-		// console.log(selectState.winner, 'wins');
-		console.log(selectState.winner);
-		// chooseWinner();
-		// addWins();
-		for (let i = 0; i < players.length; i++) {
-			// if name of winner matches player at index, add 1 to that player's number of wins
-			if (selectState.winner === players[i].name) {
-				players[i].wins = players[i].wins + 1;
-				// if name of loser matches player at index, add 1 to that player's number of losses
-			} else if (selectState.loser === players[i].name) {
-				players[i].losses = players[i].losses + 1;
-			}
-			axios.put(API_URL).then((res) => {
-				if (res.status === 200) {
-					listPlayers();
-				}
-			});
-		}
-
-		setSelectState(initialState);
-		console.log(players);
-		
-	}; */
-
-	// Copy of Handle Submit =======
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		console.log(selectState);
@@ -113,20 +61,26 @@ function Match(data, props) {
 			if (selectState.winner === playersData[i].name) {
 				playersData[i].wins = playersData[i].wins + 1;
 				// if name of loser matches player at index, add 1 to that player's number of losses
+				axios
+					.put(API_URL + `${playersData[i]._id}`, playersData[i])
+					.then((res) => {
+						if (res.status === 200) {
+							listPlayers();
+						}
+					});
 			} else if (selectState.loser === playersData[i].name) {
 				playersData[i].losses = playersData[i].losses + 1;
+				axios
+					.put(API_URL + `${playersData[i]._id}`, playersData[i])
+					.then((res) => {
+						if (res.status === 200) {
+							listPlayers();
+						}
+					});
 			}
-			axios
-				.put(API_URL + `${playersData[i].id}`, playersData[i])
-				.then((res) => {
-					if (res.status === 200) {
-						listPlayers();
-					}
-				});
 		}
 
 		setSelectState(initialState);
-		console.log(players);
 	};
 
 	const handleChange = (event) => {
